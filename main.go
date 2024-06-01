e3ec2de41c0d654c7c6663432d8d36bc2947acb8
linuxesabz <baghery.jone@gmail.com>
2024-06-01 16:38:12 +0330

    get directory from users

--- main.go
+++ main.go
@@ -7,12 +7,24 @@ import (
 )
 
 func main() {
-	entries, err := os.ReadDir("./")
+	var path1, path2 string
+	fmt.Println("Please Enter the Path Source")
+	fmt.Scanln(&path1)
+	fmt.Println("Please Enter the Path Destination")
+	fmt.Scanln(&path2)
+	entries, err := os.ReadDir(path1)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	for _, e := range entries {
-		fmt.Println(e.Name())
+		if e.IsDir() {
+			fmt.Println(e.Name())
+			Path := path2 + "\\" + e.Name()
+			err := os.MkdirAll(Path, os.ModeDir)
+			if err != nil || os.IsExist(err) {
+				log.Fatal(err)
+			}
+		}
 	}
 }
